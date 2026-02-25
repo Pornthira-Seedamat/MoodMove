36949f12c0553ce3e0ea5e1543d48965d384e51a
67021264 <67021264@up.ac.th>
2026-02-25 10:16:14 +0700

    push to cloud

---
+++ prisma/migrations/20260225024103_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day" INTEGER NOT NULL,
+    "level" INTEGER NOT NULL,
+    "color" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
