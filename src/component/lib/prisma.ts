944e9423d6d27835f2eaefaec3ae4c3537b70d84
prothira <67021129@ac.up.th>
2026-03-06 04:45:07 +0700

    Feat: Integrate Prisma Accelerate

--- src/component/lib/prisma.ts
+++ src/component/lib/prisma.ts
@@ -1,13 +1,16 @@
 // src/component/lib/prisma.ts
-// src/component/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  // เพิ่ม .$extends(withAccelerate()) ต่อท้ายตรงนี้ครับ
+  return new PrismaClient().$extends(withAccelerate());
 };
 
+type PrismaClientWithAccelerate = ReturnType<typeof prismaClientSingleton>;
+
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma: undefined | PrismaClientWithAccelerate;
 }
 
 export const prisma = globalThis.prisma ?? prismaClientSingleton();
